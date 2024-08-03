49173fffa9dd6e7ec17bdc2b89091dfff4c100d6
Sahil Agashe <sahilagashe2002@gmail.com>
2024-08-03 11:59:05 +0530

    Initial Commit

---
+++ SAA-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SAA-Demo
+//
+//  Created by SAHIL AMRUT AGASHE on 03/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SAA-Demo/SAA_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAA_DemoApp.swift
+//  SAA-Demo
+//
+//  Created by SAHIL AMRUT AGASHE on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SAA_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
